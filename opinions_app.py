d189b25d8b14f3de88b4cd6a57c2cfecaa6a5a2d
Sergey Sokolkin <hawkxdev@gmail.com>
2025-12-03 12:35:58 +0300

    chore: migrate from requirements.txt to uv

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
     return 'Совсем скоро тут будет случайное мнение о фильме!'
+
 
 if __name__ == '__main__':
     app.run()
