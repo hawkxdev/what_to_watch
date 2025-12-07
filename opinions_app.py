28a8be9e20e5c0ba24ee302b2cbb994341e1041f
Sergey Sokolkin <hawkxdev@gmail.com>
2025-12-07 18:57:03 +0300

    feat: add error pages and form validation

--- opinions_app.py
+++ opinions_app.py
@@ -1,9 +1,7 @@
-# what_to_watch/opinions_app.py
-
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
@@ -47,7 +45,7 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных записей нет.'
+        abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
@@ -57,8 +55,15 @@ def index_view():
 def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash('Такое мнение уже было оставлено ранее!')
+            return render_template('add_opinion.html', form=form)
+
         opinion = Opinion(
-            title=form.title.data, text=form.text.data, source=form.source.data
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
         )
         db.session.add(opinion)
         db.session.commit()
@@ -70,6 +75,17 @@ def add_opinion_view():
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
